80bc8e15fa6cb9374904f7654da136f5dbe09f48
Juan Andres Frigo <juan.a.frigo@gmail.com>
2023-09-29 23:52:35 -0300

    feat: se agregan validaciones al formulario

--- src/app/components/form-curso/form-curso.component.ts
+++ src/app/components/form-curso/form-curso.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Student } from 'src/app/interfaces/studen.interface';
 
 @Component({
   selector: 'app-form-curso',
@@ -7,9 +8,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./form-curso.component.scss'],
 })
 export class FormCursoComponent {
-  studentForm: FormGroup;
+  public studentForm: FormGroup = new FormGroup({});
+  public showErrors: boolean = false;
+
+  @Output() public studentOut = new EventEmitter<Student>();
 
   constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
+  }
+
+  private buildForm(): void {
     this.studentForm = this.formBuilder.group({
       controlName: ['', Validators.required],
       controlLastName: ['', Validators.required],
@@ -18,6 +26,36 @@ export class FormCursoComponent {
       controlCondition: ['', Validators.required],
     });
   }
-  
-  public onSubmit() {}
+
+  private get isStudentFormValid(): boolean {
+    return this.studentForm.valid;
+  }
+
+  private resetForm(): void {
+    this.studentForm.reset();
+  }
+
+  public isControlValid(control: string): boolean {
+    return this.studentForm.controls[`${control}`].valid;
+  }
+
+  public onSubmit(): void {
+    if (this.isStudentFormValid) {
+      this.showErrors = false;
+      console.log('valido');
+
+      const newStudent: Student = {
+        name: this.studentForm.controls['controlName'].value,
+        lastName: this.studentForm.controls['controlLastName'].value,
+        subject: this.studentForm.controls['controlSubject'].value,
+        finalNote: this.studentForm.controls['controlNote'].value,
+        condition: this.studentForm.controls['controlCondition'].value,
+      };
+
+      this.studentOut.emit(newStudent);
+      this.resetForm();
+    } else {
+      this.showErrors = true;
+    }
+  }
 }
---
+++ src/app/interfaces/studen.interface.ts
@@ -0,0 +1,7 @@
+export interface Student {
+    name:string;
+    lastName:string;
+    subject:string;
+    finalNote:string;
+    condition:string;
+}
